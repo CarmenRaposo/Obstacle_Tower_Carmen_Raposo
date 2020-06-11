9e69403b8ac5a9e69af59a41a1c1d89c7f82c50a
miguel <email@address.com>
2020-06-11 23:07:54 +0200

    Remove export_variable.sh

--- export_variable.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-export OBS_TOWER_PATH="/home/home/Data/Carmen/py_workspace/ObstacleTower_v3/ObstacleTower-v3.1/obstacletower.x86_64"
-export OBS_TOWER_RECORDINGS="/home/home/Data/Carmen/py_workspace/ObstacleTower_v3/python_scripts/My_TFM/recordings"
-export OBS_TOWER_IMAGE_LABELS="/home/home/Data/Carmen/py_workspace/ObstacleTower_v3/python_scripts/My_TFM/labels"
-export SAVE_CLASSIFIER="/home/home/Data/Carmen/py_workspace/ObstacleTower_v3/python_scripts/My_TFM/project/scripts/save_classifier.pkl"
-
-echo Varibles exported successfully
